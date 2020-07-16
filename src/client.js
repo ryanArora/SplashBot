fced699a462a744bf03a206af1d0a281d3ab8920
Ryan Arora <arora.ryan@gmail.com>
2020-07-16 15:31:08 -0400

    Renamed client.js > index.js, Renamed bot.js > client.js

--- src/client.js
+++ src/client.js
@@ -1,25 +1,12 @@
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
-const SplashBot = require('./bot.js');
+const config = require('./config.js');
+const { Client } = require('discord.js');
 
-const readdir = promisify(fs.readdir);
+class SplashBot extends Client {
+  constructor(options) {
+    super(options);
+    this.config = config;
+    this.prefix = '!';
+  }
+}
 
-const client = new SplashBot();
-
-(async () => {
-  // Event Handler
-  const evtFiles = await readdir(path.join(__dirname, 'events'));
-  console.log(`Loading a total of ${evtFiles.length} events.`);
-
-  evtFiles.forEach((file) => {
-    if (!file.endsWith('.js')) return;
-
-    const evtName = file.split('.')[0];
-    const evt = new (require('./events/' + file))(client);
-    client.on(evtName, (...args) => evt.run(...args));
-  });
-
-  // Login
-  client.login(client.config.token);
-})();
+module.exports = SplashBot;
