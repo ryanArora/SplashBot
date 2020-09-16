b04dacec53753d8118e31d2658a8601f1e802235
Ryan Arora <arora.ryan@gmail.com>
2020-09-16 01:55:59 -0400

    feat: added bots prefix to its status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,6 @@
 const ready = (client) => {
   console.log(`${client.user.tag} is serving ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`);
+  client.user.setActivity(`prefix "${client.settings.prefix}"`, { type: 'LISTENING' });
 };
 
 module.exports = ready;
